5c56fafd417bc83d46d165ecf714a647efaa3d03
Ferdinando Marcos Fonceca Maestri <ferdinando.maestri@softplan.com.br>
2022-05-11 17:06:44 -0300

    Atualizacao

---
+++ JavaScript/Aparecida Nutricao/js/principal.js
@@ -0,0 +1,11 @@
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente");
+tdpeso = paciente.querySelector(".info-peso");
+
+var peso = tdpeso.textContent;
+console.log(tdpeso); //td
+console.log(paciente);//tr
+console.log(peso);
